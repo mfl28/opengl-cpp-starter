fb7f88a22e08fe16634d92458aad1a0455acc381
Markus Fleischhacker <markus.fleischhacker28+git@gmail.com>
2020-08-23 15:59:33 +0200

    Add basic structure and fetching of GLFW library.

---
+++ cmake/fetch_glfw.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 3.14)
+
+set(GLFW_GIT_TAG "3.3.2" CACHE STRING "GLFW release version git tag.")
+
+include(FetchContent)
+
+FetchContent_Declare(
+    glfw-github
+    GIT_REPOSITORY https://github.com/glfw/glfw.git
+    GIT_TAG ${GLFW_GIT_TAG})
+
+set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(glfw-github)
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
